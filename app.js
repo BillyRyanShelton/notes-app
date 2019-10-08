8d4f3222ed0de157409be2134ac16130e694e833
Billy Shelton <sheltonbilly@utexas.edu>
2019-10-07 22:39:29 -0500

    created a command line application that add, removes, read and lists notes.

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
-const validator = require('validator');
-
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
 
-console.log(msg);
+yargs.version('1.1.0');
 
-console.log(validator.isEmail('shell.com'));
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: ()=>{
+        console.log('Adding a new note!');
+    }
+})
+
+
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: ()=>{
+        console.log('Removing the note')
+    }
+})
+
+//read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: ()=>{
+        console.log('Reading the note')
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes',
+    handler: ()=>{
+        console.log('Listing the notes')
+    }
+})
+//add, remove, read, list
+
+
+console.log(yargs.argv);
